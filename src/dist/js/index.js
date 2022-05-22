28b6996291b5b2e2c8a47f702983165ab39fe0b5
Leonardo Geja <leonardo.geja@dotgroup.com.br>
2022-05-21 23:13:05 -0300

    add header fixo and fix bug in menu mobile

--- src/dist/js/index.js
+++ src/dist/js/index.js
@@ -1,19 +1,23 @@
 const menuButtonClose = document.querySelector(".btn-mobileClose");
 const btnMobile = document.querySelector(".btn-mobile");
+const header = document.querySelector(".header");
 
 const menu = document.querySelector(".nav__list");
 function menuMobile() {
   const menuButtonClose = document.querySelector(".btn-mobileClose");
 
-  btnMobile.addEventListener("click", function () {
+  btnMobile.addEventListener("click", function (e) {
+    e.preventDefault();
     menu.classList.add("active");
     menuButtonClose.classList.add("active");
     btnMobile.classList.remove("active");
     btnMobile.style.display = "none";
     document.body.style.overflow = "hidden";
+    header.classList.remove("active");
   });
 
-  menuButtonClose.addEventListener("click", function () {
+  menuButtonClose.addEventListener("click", function (e) {
+    e.preventDefault();
     menu.classList.remove("active");
     menuButtonClose.classList.remove("active");
     btnMobile.classList.add("active");
@@ -49,3 +53,23 @@ const resizeObserver = new ResizeObserver((entries) => {
 });
 
 resizeObserver.observe(document.querySelector(".nav__list"));
+const menuLista = document.querySelectorAll("ul > li > a");
+
+menuLista.forEach((item) => {
+  item.addEventListener("click", function () {
+    menu.classList.remove("active");
+    menuButtonClose.classList.remove("active");
+    btnMobile.classList.add("active");
+    btnMobile.style.display = "block";
+    document.body.style.overflow = "auto";
+  });
+});
+
+//add scroll in header on scroll
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 100) {
+    header.classList.add("fixed");
+  } else {
+    header.classList.remove("fixed");
+  }
+});
